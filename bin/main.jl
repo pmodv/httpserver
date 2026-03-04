4e760502a14bc93fa53b07bfa675c39f66cc9983
phil vernes <phil.vernes@juliahub.com>
2026-03-04 15:58:09 -0500

    entrypoint fix

--- bin/main.jl
+++ bin/main.jl
@@ -1,2 +1,5 @@
-include("../src/httpserver.jl")
+include_relative(x) = haskey(ENV, "JULIAHUB_APP_URL") ? include(joinpath(pwd(), "bin", x)) : include(x)
+
+include_relative("httpserver.jl")
+
 httpserver.bootServer()
